34dcbfe2c3ddd291067d22e541931bbe8ff29f0a
JD1174 <53028466+JD1174@users.noreply.github.com>
2026-02-08 12:40:06 -0500

    filled in both assignments

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,12 +1,17 @@
 
 class Book:
     def __init__(self, author, title):
-        pass
+        self.author = author
+        self.title = title
     
     def display(self):
-        pass
-
+        print(f"{self.title}, written by {self.author}")
 
 if __name__ == "__main__":
-    a = Book("Thorpe", "Beat the Dealer")
-    print("This is a assignment 1 part 2")
+    # Instantiate the two objects
+    book1 = Book("J. K. Rowling", "Harry Potter and the Goblet of Fire")
+    book2 = Book("Walter Scott", "Ivanhoe: A Romance")
+    
+    # Print
+    book1.display()
+    book2.display()
